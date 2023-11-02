c8b4b2ca03b1d245140809e973b06ecb4185dfc7
nikita <nikita.surzhin@gmail.com>
2023-11-02 20:13:30 +0300

    added  -calculator.py  -test.py

---
+++ tests/test.py
@@ -0,0 +1,22 @@
+import pytest
+
+from app.calculator import Calculator
+
+class TestCalculator:
+    def setup(self):
+        self.calc = Calculator
+
+    def test_adding_success(self):
+        assert self.calc.adding(self, 2, 2) == 2
+
+    def test_subtraction(self):
+        assert self.calc.subtraction(self, 20, 5) == 15
+
+    def test_division(self):
+         assert self.calc.division(self, 50, 2) == 25
+
+    def test_multiply(self):
+        assert self.calc.multiply(self, 32, 2) == 64
+
+    def teardown(self):
+        print('Выполнения метода Teardown')
